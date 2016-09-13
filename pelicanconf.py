eb72e6f2557d2229399888630d59563cd2e71531
uid-lsheng <lisha.sheng@gmail.com>
2016-09-13 21:22:13 +0800

    Add files via upload

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Lisa'
+SITENAME = u'Halfway Coder'
+SITEURL = ''
+THEME = 'pelican-themes/gum'
+PATH = 'content'
+
+TIMEZONE = 'Asia/Singapore'
+DISQUS_SITENAME = 'disqus_bvaZ5b7pms'
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
